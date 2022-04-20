0b5133f958def5171c336a7159d9d54e0e647c24
ErsultanKanapiya <91111793+ErsultanKanapiya@users.noreply.github.com>
2022-04-20 15:17:13 +0600

    Update settings.py

--- SneakerLab/settings.py
+++ SneakerLab/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-drglgb#g*sf#79yy8le$aa19ml$*%%*zpiyi(%+wnv9u=u5cpn'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
