3fd69676fd02c7043793e26f3a80b500a80646c8
ErsultanKanapiya <200103091@stu.sdu.edu.kz>
2022-04-22 04:41:02 +0600

    comment

---
+++ main/forms.py
@@ -0,0 +1,9 @@
+# from dataclasses import field
+# from unicodedata import category
+# from django import forms
+# from .models import ProductCategories
+
+# class ProductCategoryForm(forms.ModelForm):
+#     class Meta:
+#         model = category
+#         fields = ['product_category']
