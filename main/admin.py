639f2cb4e4cbb08f342dfcfd579f604156eb892e
ErsultanKanapiya <91111793+ErsultanKanapiya@users.noreply.github.com>
2022-04-22 04:06:39 +0600

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contact 
+from .models import Contact, SneakerForMan, SneakerForWoman, SneakerForKid
 
-admin.site.register(Contact )
+admin.site.register(Contact)
+admin.site.register(SneakerForMan)
+admin.site.register(SneakerForWoman)
+admin.site.register(SneakerForKid)
