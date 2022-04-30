04a0f63f64ddb0a2d778e0b168320c52f99df002
ErsultanKanapiya <91111793+ErsultanKanapiya@users.noreply.github.com>
2022-05-01 05:15:06 +0600

    Delete main directory

--- main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Contact, SneakerForMan, SneakerForWoman, SneakerForKid
-
-admin.site.register(Contact)
-admin.site.register(SneakerForMan)
-admin.site.register(SneakerForWoman)
-admin.site.register(SneakerForKid)
--- main/forms.py
+++
@@ -1,9 +0,0 @@
-# from dataclasses import field
-# from unicodedata import category
-# from django import forms
-# from .models import ProductCategories
-
-# class ProductCategoryForm(forms.ModelForm):
-#     class Meta:
-#         model = category
-#         fields = ['product_category']
